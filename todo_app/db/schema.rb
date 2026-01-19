74d58fba5c672a8a7a18b1c164e8620bcbbc7d13
Debora Viana <deborasviana3@gmail.com>
2026-01-19 11:26:32 -0300

    ✨feat(backend): implementa CRUD de Tarefas

--- todo_app/db/schema.rb
+++ todo_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_19_073008) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_19_140038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -30,6 +30,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_19_073008) do
     t.index ["board_id"], name: "index_lists_on_board_id"
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.bigint "list_id", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
@@ -40,4 +49,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_19_073008) do
 
   add_foreign_key "boards", "users"
   add_foreign_key "lists", "boards"
+  add_foreign_key "tasks", "lists"
 end
