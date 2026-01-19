985026b19dbe192d6e97f53c1bdde13dab1472e0
Debora Viana <deborasviana3@gmail.com>
2026-01-19 04:43:16 -0300

    ✨feat(backend): implementa CRUD de Listas

---
+++ todo_app/app/controllers/lists_controller.rb
@@ -0,0 +1,59 @@
+class ListsController < ApplicationController
+  before_action :set_board
+  before_action :set_list, only: %i[show edit update destroy]
+
+  def index
+    @lists = @board.lists
+  end
+
+  def show
+  end
+
+  def new
+    @list = @board.lists.new
+  end
+
+  def create
+    @list = @board.lists.new(list_params)
+
+    if @list.save
+      redirect_to [@board, @list], notice: "Lista criada com sucesso."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to [@board, @list], notice: "Lista atualizada com sucesso."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to board_lists_path(@board), notice: "Lista excluída com sucesso."
+  end
+
+  private
+
+  def set_board
+    @board = current_user.boards.find(params[:board_id])
+  end
+
+  def set_list
+    @list = @board.lists.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:title)
+  end
+
+  def current_user
+    User.find_by!(email: "dev@local")
+  end
+end
--- todo_app/db/schema.rb
+++ todo_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_19_064724) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_19_073008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_19_064724) do
     t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
+  create_table "lists", force: :cascade do |t|
+    t.bigint "board_id", null: false
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_lists_on_board_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
@@ -31,4 +39,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_19_064724) do
   end
 
   add_foreign_key "boards", "users"
+  add_foreign_key "lists", "boards"
 end
