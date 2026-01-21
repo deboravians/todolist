1c909b620901fe7cc0328a1dc440242bd6e9da3b
Debora Viana <deborasviana3@gmail.com>
2026-01-21 00:17:59 -0300

    ✨feat(frontend): add telas de quadros e de visualização por status

--- todo_app/app/controllers/application_controller.rb
+++ todo_app/app/controllers/application_controller.rb
@@ -5,6 +5,14 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
   allow_browser versions: :modern
 
+  def after_sign_in_path_for(resource)
+    boards_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 
